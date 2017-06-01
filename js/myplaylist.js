462c6b35741ddb90f5acfbda6707df63ac10013a
leonardo173 <leonardo.173@hotmail.com>
2017-06-01 17:13:13 -0300

    Primeira versao
    primeiro projeto sobre carrosel de musica.

---
+++ js/myplaylist.js
@@ -0,0 +1,135 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/01-Battleme-Hey-Hey-My.mp3',
+        
+        title:'Hey Hey my',
+        artist:'Battleme',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'2:56',
+        cover:'mix/thumn.jpg'
+    },
+
+    {
+        mp3:'mix/02-XXXTENTACION- Look At Me!.mp3',
+        title:'Look At Me!',
+        artist:'XXXTENTACION',
+        rating:4,
+        buy:'#',
+        price:'80.00',
+        duration:'3:30',
+        cover:'mix/02-XXX.jpg'
+    },
+    {
+        mp3:'mix/03-Jack-Savoretti-Soldiers-Eyes.mp3',
+        
+        title:'Jack Savoretti',
+        artist:'Soldiers Eyes',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'3:07',
+        cover:'mix/thumn.jpg'
+    },
+    {
+        mp3:'mix/Suicide Boys - Opana LEGENDADO by FalcaoSombrio ( $UICIDEBOY$ - O PANA! ).mp3',
+        title:'OPANA',
+        artist:'$uicide boys',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'3:07',
+        cover:'mix/04-$$$.jpg'
+    },
+    {
+        mp3:'mix/05-The-White-Buffalo-The-House-of-The-Rising-Sun.mp3',
+        title:'The House of The Rising Sun',
+        artist:'The White Buffalo',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'5:22',
+        cover:'mix/thumn.jpg'
+    },
+    {
+        mp3:'mix/06-Ultimate.mp3',
+        title:'Ultimate',
+        artist:'Denzel Curry',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'5:22',
+        cover:'mix/curry.jpg'
+    },
+    {
+        mp3:'mix/07 Bring me the horizon Suicide Season.mp3',
+        title:'Suicide Season',
+        artist:'Bring the Horizon',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'8:17',
+        cover:'mix/07 Bring me the horizon Suicide Season.jpg'
+    },
+    {
+        mp3:'mix/08-$UICIDEBOY$- FUCKTHEPOPULATION.mp3',
+        title:'FUCKTHEPOPULATION',
+        artist:'$uicide boys',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'8:17',
+        cover:'mix/08-sucide-boys.jpg'
+    },
+    {
+        mp3:'mix/08-$UICIDEBOY$- FUCKTHEPOPULATION.mp3',
+        title:'FUCKTHEPOPULATION',
+        artist:'$uicide boys',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'8:17',
+        cover:'mix/08-suicide-boys.jpg'
+    },
+    {
+        mp3:'mix/09-Eden - Drugs.mp3',
+        title:'Drugs',
+        artist:'Eden',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'5:45',
+        cover:'mix/09- Eden.jpg'
+    },
+    {
+        mp3:'mix/11-spooky black - reason.mp3',
+        title:'Reason',
+        artist:'Spook black',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'5:45',
+        cover:'mix/11-Spook.jpg'
+    }
+
+
+
+
+
+
+
+
+
+
+
+];
